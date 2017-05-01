5649d6870c7665e21433384ec609ecbb5d25e105
Rafa Gago <rafael_gago_81@hotmail.com>
2017-05-01 11:18:54 +0200

    add compressed entry ptr + timestamp

--- src/malc/malc.c
+++ src/malc/malc.c
@@ -1,4 +1,3 @@
-#include <string.h>
 #include <stdarg.h>
 
 #include <malc/malc.h>
@@ -16,6 +15,7 @@
 #include <malc/cfg.h>
 #include <malc/stack_args.h>
 #include <malc/memory.h>
+#include <malc/log_entry.h>
 
 #ifdef __cplusplus
   extern "C" {
@@ -47,16 +47,23 @@ struct malc {
 };
 /*----------------------------------------------------------------------------*/
 enum queue_command {
-  q_cmd_dealloc = 0,
+  q_cmd_timestamped_entry = 0,
+  q_cmd_entry,
+  q_cmd_dealloc,
   q_cmd_flush,
-  q_cmd_terminate,
   q_cmd_max,
 };
 /*----------------------------------------------------------------------------*/
+typedef struct info_byte {
+  u8 cmd : 8 - alloc_tag_bits;
+  u8 tag : alloc_tag_bits;
+}
+info_byte;
+/*----------------------------------------------------------------------------*/
 typedef struct qnode {
   mpsc_i_node hook;
-  uword  data;
-  u8     slots;
+  info_byte   info;
+  u8          slots;
   /* would be nice to have flexible arrays in C++ */
 }
 qnode;
@@ -81,144 +88,25 @@ static void malc_tls_destructor (void* mem, void* context)
   malc*  l = (malc*) context;
   qnode* n = (qnode*) mem;
   n->slots = 0;
-  n->data  = q_cmd_dealloc;
+  n->info.cmd = q_cmd_dealloc;
   mpsc_i_node_set (&n->hook, nullptr, 0, 0);
   mpsc_i_produce_notag (&l->q, &n->hook);
 }
 /*----------------------------------------------------------------------------*/
-static void malc_send_blocking_cmd (malc* l, uword cmd)
+static void malc_send_blocking_flush (malc* l)
 {
  /*TODO: use the heap and provide the user a timeout*/
   qnode            n;
   nonblock_backoff b;
 
   n.slots = 0;
-  n.data  = cmd;
+  n.info.cmd = q_cmd_flush;
   mpsc_i_node_set (&n.hook, nullptr, 0, 0);
   mpsc_i_produce_notag (&l->q, &n.hook);
 
   nonblock_backoff_init (&b, 10, 15, 1, 2, 1, 100);
   while (n.slots == 0) {
     nonblock_backoff_run (&b);
-  }
-}
-/*----------------------------------------------------------------------------*/
-static void malc_encode (u8* mem, va_list vargs, malc_const_entry const* e)
-{
-  char const* partype = &e->info[1];
-
-  uword compressed_header_bytes = (e->compressed_count + 1) & ~u_lsb_set (1);
-  for (uword i = 0; i < compressed_header_bytes; ++i) {
-    mem[i] = 0;
-  }
-  uword compress_idx = 0;
-  u8* wptr = mem + compressed_header_bytes;
-  /* the compiler will remove the fixed-size memcpy calls*/
-  while (*partype) {
-    switch (*partype) {
-    case malc_type_i8:
-    case malc_type_u8: {
-      u8 v = malc_get_va_arg (vargs, v);
-      memcpy (wptr, &v, sizeof v);
-      wptr += sizeof v;
-      break;
-      }
-    case malc_type_i16:
-    case malc_type_u16: {
-      u16 v = malc_get_va_arg (vargs, v);
-      memcpy (wptr, &v, sizeof v);
-      wptr += sizeof v;
-      break;
-      }
-#ifdef MALC_NO_BUILTIN_COMPRESSION
-    case malc_type_i32:
-    case malc_type_u32: {
-      u32 v = malc_get_va_arg (vargs, v);
-      memcpy (wptr, &v, sizeof v);
-      wptr += sizeof v;
-      break;
-      }
-#else
-    case malc_type_i32:
-    case malc_type_u32: {
-      malc_compressed_32 v;
-      v = malc_get_va_arg (vargs, v);
-      uword size = (v.format_nibble & ((1 << 3) - 1)) + 1;
-      bl_assert (size <= sizeof (u32));
-      mem[compress_idx / 2] |= (u8) v.format_nibble << ((compress_idx & 1) * 4);
-      ++compress_idx;
-      for (uword i = 0; i < size; ++i) {
-        *wptr = (u8) (v.v >> (i * 8));
-        ++wptr;
-      }
-      break;
-      }
-#endif
-    case malc_type_float: {
-      float v = malc_get_va_arg (vargs, v);
-      memcpy (wptr, &v, sizeof v);
-      wptr += sizeof v;
-      break;
-      }
-#ifdef MALC_NO_BUILTIN_COMPRESSION
-    case malc_type_i64:
-    case malc_type_u64: {
-      u64 v = malc_get_va_arg (vargs, v);
-      memcpy (wptr, &v, sizeof v);
-      wptr += sizeof v;
-      break;
-      }
-#else
-    case malc_type_i64:
-    case malc_type_u64: {
-      malc_compressed_64 v;
-      v = malc_get_va_arg (vargs, v);
-      uword size = (v.format_nibble & ((1 << 3) - 1)) + 1;
-      bl_assert (size <= sizeof (u64));
-      mem[compress_idx / 2] |= (u8) v.format_nibble << ((compress_idx & 1) * 4);
-      ++compress_idx;
-      for (uword i = 0; i < size; ++i) {
-        *wptr = (u8) (v.v >> (i * 8));
-        ++wptr;
-      }
-      break;
-    }
-#endif
-    case malc_type_double: {
-      double v = malc_get_va_arg (vargs, v);
-      memcpy (wptr, &v, sizeof v);
-      wptr += sizeof v;
-      break;
-      }
-    case malc_type_ptr: {
-      void* v = malc_get_va_arg (vargs, v);
-      memcpy (wptr, &v, sizeof v);
-      wptr += sizeof v;
-      break;
-      }
-    case malc_type_lit: {
-      malc_lit v = malc_get_va_arg (vargs, v);
-      memcpy (wptr, &v.lit, sizeof v);
-      wptr += sizeof v;
-      break;
-      }
-    case malc_type_str:
-    case malc_type_bytes: {
-      malc_mem v = malc_get_va_arg (vargs, v);
-      if (unlikely (!v.mem)) {
-        v.size = 0;
-      }
-      memcpy (wptr, &v.size, sizeof v.size);
-      wptr += sizeof v.size;
-      memcpy (wptr, v.mem, v.size);
-      wptr += v.size;
-      break;
-      }
-    default: {
-      bl_assert (0 && "bug");
-      }
-    }
-    ++partype;
   }
 }
 /*----------------------------------------------------------------------------*/
@@ -274,6 +162,7 @@ MALC_EXPORT bl_err malc_get_cfg (malc* l, malc_cfg* cfg)
 MALC_EXPORT bl_err malc_init (malc* l, malc_cfg const* cfg)
 {
   /* TODO: cfg validation + data validation on each destination */
+  /* TODO: cfg validation: sanitize producer.timestamp to 1 or 0 */
   uword expected = st_stopped;
   if (atomic_uword_strong_cas_rlx (&l->state, &expected, st_initializing)) {
     return bl_preconditions;
@@ -289,7 +178,7 @@ MALC_EXPORT bl_err malc_flush (malc* l)
   if (current != st_running) {
     return bl_preconditions;
   }
-  malc_send_blocking_cmd (l, q_cmd_flush);
+  malc_send_blocking_flush (l);
   return bl_ok;
 }
 /*----------------------------------------------------------------------------*/
@@ -299,7 +188,7 @@ MALC_EXPORT bl_err malc_terminate (malc* l)
   if (atomic_uword_strong_cas_rlx (&l->state, &expected, st_terminating)) {
     return bl_preconditions;
   }
-  malc_send_blocking_cmd (l, q_cmd_terminate);
+  malc_send_blocking_flush (l);
   nonblock_backoff b;
   nonblock_backoff_init_default (&b, 1000);
   while (atomic_uword_load_rlx (&l->state) == st_stopped) {
@@ -350,33 +239,30 @@ MALC_EXPORT bl_err malc_run_consume_task (malc* l, uword timeout_us)
     now = bl_get_tstamp();
     if (likely (!err)) {
       qnode* n = to_type_containing (qn, hook, qnode);
-      if (likely (n->data >= q_cmd_max)) {
-        alloc_tag               tag;
-        tag = n->data & alloc_tag_mask;
-        /* unused warning
-        malc_const_entry const* ent;
-        ent = (malc_const_entry const*) (n->data & ~alloc_tag_mask);
-        */
+      bool has_tstamp = false;
+      switch (n->info.cmd) {
+      case q_cmd_timestamped_entry:
+        has_tstamp = true; /* deliberate fall-through*/
+
+      case q_cmd_entry: {
+        alloc_tag tag = n->info.tag;
+
         /* TODO, decode and send to all destinations */
         memory_dealloc (&l->mem, (u8*) n, tag, ((u32) n->slots) + 1);
-      }
-      else {
-        switch (n->data) {
-        case q_cmd_dealloc:
-          bl_dealloc (l->alloc, n);
-          break;
-        case q_cmd_flush:
-          /*TODO: call flush on all the destinations*/
-          ++n->slots;
-          break;
-        case q_cmd_terminate:
-          terminated = true;
-          /*TODO: call flush on all the destinations*/
-          ++n->slots;
-          break;
-        default:
-          bl_assert (0 && "bug or malicious code");
+        break;
         }
+      case q_cmd_dealloc:
+        bl_dealloc (l->alloc, n);
+        break;
+
+      case q_cmd_flush:
+        /*TODO: call flush on all the destinations*/
+        ++n->slots;
+        terminated = atomic_uword_load_rlx (&l->state) == st_terminating;
+        break;
+
+      default:
+        bl_assert (0 && "bug or malicious code");
       }
       nonblock_backoff_init_default (&l->cbackoff, l->consumer.backoff_max_us);
     }
@@ -430,15 +316,15 @@ MALC_EXPORT uword malc_get_min_severity (malc const* l)
 }
 /*----------------------------------------------------------------------------*/
 MALC_EXPORT bl_err malc_log(
-  malc* l, malc_const_entry const* e, uword payload_size, ...
+  malc* l, malc_const_entry const* entry, uword payload_size, ...
   )
 {
 #ifdef MALC_SAFETY_CHECK
   if (unlikely (
     !l ||
-    !e ||
-    !e->format ||
-    !e->info
+    !entry ||
+    !entry->format ||
+    !entry->info
     )) {
   /* code triggering this "bl_invalid" is either not using the macros or
      malicious*/
@@ -447,38 +333,39 @@ MALC_EXPORT bl_err malc_log(
 #else
   bl_assert(
     l &&
-    e &&
-    e->format &&
-    e->info
+    entry &&
+    entry->format &&
+    entry->info
     );
 #endif
   if (unlikely (atomic_uword_load_rlx (&l->state)) != st_running) {
     return bl_preconditions;
   }
-  alloc_tag tag;
-  uword size  = sizeof (qnode) + payload_size;
-  size       += (e->compressed_count + 1) & ~u_lsb_set (1);
-  u8* mem     = nullptr;
+  entry_encoder ee;
+  entry_encoder_init (&ee, entry, l->producer.timestamp);
+  uword size  = sizeof (qnode) + entry_encoder_entry_size (&ee, payload_size);
   uword slots = div_ceil (size, alloc_slot_size);
   if (unlikely (slots) > (1 << (sizeof_member (qnode, slots) * 8))) {
     /*entries are limited at 8KB*/
     return bl_range;
   }
+  alloc_tag tag;
+  u8* mem    = nullptr;
   bl_err err = memory_alloc (&l->mem, &mem, &tag, slots);
   if (unlikely (err)) {
     return err;
   }
   va_list vargs;
   va_start (vargs, payload_size);
-  /*malc_encode  is deliberately unsafe, if the log macros are used everything
-    will be correct. Checks have to be avoided on the fast-path*/
-  malc_encode (mem + sizeof (qnode), vargs, e);
+  /*entry_encoder_execute is deliberately unsafe (avoid branches on the
+    fast-path), if the log macros are used everything will be correct. Checks
+    have to be avoided on the fast-path*/
+  entry_encoder_execute (&ee, mem + sizeof (qnode), vargs);
   va_end(vargs);
-  qnode* n = (qnode*) mem;
-  n->slots = slots - 1;
-  n->data  = (uword) e;
-  bl_assert ((n->data & ~alloc_tag_mask) == n->data);
-  n->data |= tag & alloc_tag_mask;
+  qnode* n    = (qnode*) mem;
+  n->slots    = slots - 1;
+  n->info.cmd = l->producer.timestamp ? q_cmd_timestamped_entry : q_cmd_entry;
+  n->info.tag = tag;
   mpsc_i_node_set (&n->hook, nullptr, 0, 0);
   mpsc_i_produce_notag (&l->q, &n->hook);
   return bl_ok;
