d8f5738dc532c0e7744e928cdd5d2ce9062840d9
Rafa Gago <rafael_gago_81@hotmail.com>
2017-12-22 17:07:52 +0100

    interface: malc private include has to be the first one

--- include/malc/malc.h
+++ include/malc/malc.h
@@ -13,11 +13,12 @@
   #include <malc/config.h>
 #endif
 
+/* has to be the first, defines preprocessor macros */
+#include <malc/impl/malc_private.h>
 #include <malc/libexport.h>
 #include <malc/cfg.h>
 #include <malc/destination.h>
 #include <malc/impl/serializer.h>
-#include <malc/impl/malc_private.h>
 
 struct malc;
 typedef struct malc malc;
